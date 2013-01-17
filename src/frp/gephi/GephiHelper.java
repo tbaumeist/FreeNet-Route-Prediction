21004d18c75eeae9038c2b797efb11e96178c7cc
Todd Baumeister <tbaumeist@gmail.com>
2013-01-17 14:06:45 -0800

    Added functionality to load a gephi graph from an input stream

--- src/frp/gephi/GephiHelper.java
+++ src/frp/gephi/GephiHelper.java
@@ -1,13 +1,19 @@
 package frp.gephi;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.file.Paths;
 
 import org.gephi.graph.api.DirectedGraph;
 import org.gephi.graph.api.GraphController;
 import org.gephi.graph.api.GraphModel;
 import org.gephi.io.importer.api.Container;
 import org.gephi.io.importer.api.EdgeDefault;
+import org.gephi.io.importer.api.FileType;
 import org.gephi.io.importer.api.ImportController;
+import org.gephi.io.importer.plugin.file.ImporterBuilderDOT;
+import org.gephi.io.importer.spi.FileImporterBuilder;
 import org.gephi.io.processor.plugin.DefaultProcessor;
 import org.gephi.project.api.ProjectController;
 import org.gephi.project.api.Workspace;
@@ -15,7 +21,13 @@ import org.openide.util.Lookup;
 
 public class GephiHelper { 
 
-	public DirectedGraph loadGraphFile(String topologyFileName) throws Exception{
+	public DirectedGraph loadGraphFile(String topologyFileName) throws Exception{   
+		File file = new File(topologyFileName);
+        FileInputStream input = new FileInputStream(file);
+        return loadGraphFile(input, getExtension(file));
+	}
+	
+	public DirectedGraph loadGraphFile(InputStream topologyFile, String fileExt) throws Exception{
 		//Init a project - and therefore a workspace
         ProjectController pc = Lookup.getDefault().lookup(ProjectController.class);
         pc.newProject();
@@ -23,10 +35,12 @@ public class GephiHelper {
 
         GraphModel graphModel = Lookup.getDefault().lookup(GraphController.class).getModel();
         ImportController importController = Lookup.getDefault().lookup(ImportController.class);
-
+        FileImporterBuilder importBuilder = getMatchingImporter(fileExt);
+        if(importBuilder == null)
+        	throw new Exception("Unable to find file importer for the given file type : " + fileExt);
+        
         //Import file       
-        File file = new File(topologyFileName);
-        Container container = importController.importFile(file);
+        Container container = importController.importFile(topologyFile, importBuilder.buildImporter());
         container.getLoader().setEdgeDefault(EdgeDefault.DIRECTED);   //Force DIRECTED
 
         //Append imported data to GraphAPI
@@ -35,4 +49,31 @@ public class GephiHelper {
         //See if graph is well imported
         return graphModel.getDirectedGraph();
 	}
+	
+	private String getExtension(File f) {
+	    String ext = null;
+	    String s = f.getName();
+	    int i = s.lastIndexOf('.');
+
+	    if (i > 0 &&  i < s.length() - 1) {
+	        ext = s.substring(i+1).toLowerCase();
+	    }
+	    return ext;
+	}
+	
+    private FileImporterBuilder getMatchingImporter(String extension) {
+    	if(!extension.startsWith("."))
+    		extension = "." + extension;
+    	FileImporterBuilder[]fileImporterBuilders = Lookup.getDefault().lookupAll(FileImporterBuilder.class).toArray(new FileImporterBuilder[0]);
+        for (FileImporterBuilder im : fileImporterBuilders) {
+            for (FileType ft : im.getFileTypes()) {
+                for (String ext : ft.getExtensions()) {
+                    if (ext.equalsIgnoreCase(extension)) {
+                        return im;
+                    }
+                }
+            }
+        }
+        return null;
+    }
 }
